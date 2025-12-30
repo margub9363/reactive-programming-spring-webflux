af6a04dce41f14caff247113fa54d554ea49dab2
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-30 10:53:46 +0530

    Show SQL

--- webflux-playground/src/test/java/com/rahman/webflux_playground/sec02_r2dbc/AbstractTest.java
+++ webflux-playground/src/test/java/com/rahman/webflux_playground/sec02_r2dbc/AbstractTest.java
@@ -2,6 +2,7 @@ package com.rahman.webflux_playground.sec02_r2dbc;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest (properties = "sec=sec02_r2dbc")
+@SpringBootTest(properties = {"sec=sec02_r2dbc",
+        "logging.level.org.springframework.r2dbc=DEBUG"})
 public abstract class AbstractTest {
 }
