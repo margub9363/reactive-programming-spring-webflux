defc8c543ff0c6211d3ff169722518b6d434ed46
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-30 10:37:45 +0530

    Configuring Tests

--- webflux-playground/src/main/java/com/rahman/webflux_playground/sec02_r2dbc/CustomerRepository.java
+++ webflux-playground/src/main/java/com/rahman/webflux_playground/sec02_r2dbc/CustomerRepository.java
@@ -3,8 +3,11 @@ package com.rahman.webflux_playground.sec02_r2dbc;
 import com.rahman.webflux_playground.sec02_r2dbc.entity.Customer;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+    Flux<Customer> findByName(String name);
 
 }
--- webflux-playground/src/test/java/com/rahman/webflux_playground/sec02_r2dbc/Lec01CustomerRepositoryTest.java
+++ webflux-playground/src/test/java/com/rahman/webflux_playground/sec02_r2dbc/Lec01CustomerRepositoryTest.java
@@ -1,14 +1,16 @@
 package com.rahman.webflux_playground.sec02_r2dbc;
 
+import com.rahman.webflux_playground.sec02_r2dbc.entity.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.test.StepVerifier;
 
+@Slf4j
 public class Lec01CustomerRepositoryTest extends AbstractTest {
-
-    private static final Logger log = LoggerFactory.getLogger(Lec01CustomerRepositoryTest.class);
 
     @Autowired
     private CustomerRepository repository;
@@ -19,6 +21,39 @@ public class Lec01CustomerRepositoryTest extends AbstractTest {
                 .doOnNext(c -> log.info("{}", c))
                 .as(StepVerifier::create)
                 .expectNextCount(10)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void findById() {
+        this.repository.findById(2)
+                .doOnNext(c -> log.info("{}", c))
+                .as(StepVerifier::create)
+                .assertNext(c -> Assertions.assertEquals("mike", c.getName()))
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void findByName(){
+        this.repository.findByName("jake")
+                .doOnNext(c -> log.info("{}", c))
+                .as(StepVerifier::create)
+                .assertNext(c -> Assertions.assertEquals("jake@gmail.com", c.getEmail()))
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void insertAndDeleteCustomer() {
+        var customer = new Customer();
+        customer.setName("marshal");
+        customer.setEmail("marshal@gmail.com");
+        this.repository.save(customer)
+                .doOnNext(c -> log.info("{}", c))
+                .as(StepVerifier::create)
+                .assertNext(c -> Assertions.assertNotNull(c.getId()))
                 .expectComplete()
                 .verify();
     }
