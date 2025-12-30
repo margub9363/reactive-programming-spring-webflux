fa1c0914e256c8213edc8409f0f968373f06ae85
margub9363 <69838623+margub9363@users.noreply.github.com>
2025-12-30 18:11:36 +0530

    Integration Test Part 1

---
+++ webflux-playground/src/test/java/com/rahman/webflux_playground/sec03/CustomerServiceTest.java
@@ -0,0 +1,45 @@
+package com.rahman.webflux_playground.sec03;
+
+import com.rahman.webflux_playground.sec03.dto.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@Slf4j
+@AutoConfigureWebTestClient
+@SpringBootTest(properties = "sec=sec03")
+public class CustomerServiceTest {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    public void allCustomers() {
+        this.client.get()
+                .uri("/customers")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(CustomerDto.class)
+                .value(list -> log.info("{}", list))
+                .hasSize(10);
+    }
+
+    @Test
+    public void paginatedCustomers() {
+        this.client.get()
+                .uri("/customers/paginated?page=3&size=2")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .consumeWith(r -> log.info("{}", new String(r.getResponseBody())))
+                .jsonPath("$.length()").isEqualTo(2)
+                .jsonPath("$[0].id").isEqualTo(5)
+                .jsonPath("$[1].id").isEqualTo(6);
+
+    }
+}
